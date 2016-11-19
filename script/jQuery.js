3a6ca6ed09f1f233ae257b02ae950b75e19deec1
oddeko <oddeko@stud.ntnu.no>
2016-11-19 02:20:50 +0100

    fjernet værmelding fra side2
    legges til på alle andre

--- script/jQuery.js
+++ script/jQuery.js
@@ -12,35 +12,34 @@ setInterval(function() {
 $(document).ready(function(){
     $("#side1").click(function(){
 		$("#slideshow").show();
+		$(".col-sm-2").show();
     });
 });
 $(document).ready(function(){
     $("#side2").click(function(){
 		$("#slideshow").hide();
+		$(".col-sm-2").hide();
         $("#div1").load("sidene/2.html");
     });
 });
 $(document).ready(function(){
     $("#side3").click(function(){
 		$("#slideshow").hide();
+		$(".col-sm-2").show();
         $("#div1").load("sidene/3.html");
     });
 });
 $(document).ready(function(){
     $("#side4").click(function(){
 		$("#slideshow").show();
+		$(".col-sm-2").show();
         $("#div1").load("sidene/4.html");
     });
 });
 $(document).ready(function(){
     $("#side5").click(function(){
 		$("#slideshow").show();
-        $("#div1").load("sidene/5.html");
-    });
-});
-$(document).ready(function(){
-    $("#side5").click(function(){
-		$("#slideshow").show();
+		$(".col-sm-2").show();
         $("#div1").load("sidene/5.html");
     });
 });
@@ -57,6 +56,7 @@ $(this).stop().animate({"opacity": "1.0"});
 $(document).ready(function(){
     $(".skog").click(function(){
 		$("#slideshow").hide();
+		$(".col-sm-2").show();
         $("#div1").load("sidene/skog.html");
     });
 });
@@ -64,6 +64,7 @@ $(document).ready(function(){
 $(document).ready(function(){
     $(".fjell").click(function(){
 		$("#slideshow").hide();
+		$(".col-sm-2").show();
         $("#div1").load("sidene/fjell.html");
     });
 });
