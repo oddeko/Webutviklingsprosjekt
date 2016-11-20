3d69ea19d56a3c64591d8e1b319842aa2eef1bb2
oddeko <oddeko@stud.ntnu.no>
2016-11-20 15:40:25 +0100

    Ryddet og kommenterte

--- script/jQuery.js
+++ script/jQuery.js
@@ -1,6 +1,7 @@
 // JavaScript Document
-$("#slideshow > div:first").hide();
 
+// SLIDESHOW
+$("#slideshow > div:first").hide();
 setInterval(function() {
   $('#slideshow > div:first')
     .animate(2000)
@@ -9,12 +10,14 @@ setInterval(function() {
     .end()
     .appendTo('#slideshow');
 },  3000);
+// HOVEDSIDE
 $(document).ready(function(){
     $("#side1").click(function(){
 		$("#slideshow").show();
 		$(".col-sm-2").show();
     });
 });
+// SIDE2
 $(document).ready(function(){
     $("#side2").click(function(){
 		$("#slideshow").hide();
@@ -22,6 +25,7 @@ $(document).ready(function(){
         $("#div1").load("sidene/2.html");
     });
 });
+// SIDE3
 $(document).ready(function(){
     $("#side3").click(function(){
 		$("#slideshow").hide();
@@ -29,6 +33,7 @@ $(document).ready(function(){
         $("#div1").load("sidene/3.html");
     });
 });
+// SIDE4
 $(document).ready(function(){
     $("#side4").click(function(){
 		$("#slideshow").show();
@@ -36,6 +41,7 @@ $(document).ready(function(){
         $("#div1").load("sidene/4.html");
     });
 });
+// SIDE5
 $(document).ready(function(){
     $("#side5").click(function(){
 		$("#slideshow").show();
@@ -43,16 +49,15 @@ $(document).ready(function(){
         $("#div1").load("sidene/5.html");
     });
 });
-
+// BILDEEFFEKT
 $(document).ready(function(){
-$(".img-rounded").hover(function(){
-$(this).stop().animate({"opacity": "0.85"});
-}, function(){
-$(this).stop().animate({"opacity": "1.0"});
+	$(".img-rounded").hover(function(){
+		$(this).stop().animate({"opacity": "0.85"});
+		}, function(){
+		$(this).stop().animate({"opacity": "1.0"});
+	});
 });
-
-});
-
+// SIDE-SKOG
 $(document).ready(function(){
     $(".skog").click(function(){
 		$("#slideshow").hide();
@@ -60,7 +65,7 @@ $(document).ready(function(){
         $("#div1").load("sidene/skog.html");
     });
 });
-
+// SIDE-FJELL
 $(document).ready(function(){
     $(".fjell").click(function(){
 		$("#slideshow").hide();
