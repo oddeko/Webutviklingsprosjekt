bd2e50e4cfe1a6deb0a2a2bef8a8b2139c4d3cc5
oddeko <oddeko@stud.ntnu.no>
2016-11-21 15:22:56 +0100

    Gjorde om fra ID til class

--- script/jQuery.js
+++ script/jQuery.js
@@ -12,16 +12,16 @@ setInterval(function() {
 },  3000);
 // HOVEDSIDE
 $(document).ready(function(){
-    $("#side1").click(function(){
+    $(".side1").click(function(){
 		$("#slideshow").show();
 		$(".col-sm-2").show();
     });
 });
 // SIDE2
 $(document).ready(function(){
-    $("#side2").click(function(){ // Utfører gitte handlinger ved klikk på element med ID: "side2".
+    $(".side2").click(function(){ // Utfører gitte handlinger ved klikk på element med ID: "side2".
 		$("li").removeClass("active"); // Fjerner klassen "active" fra elementer med li(ste)-tag.
-		$("#side2").addClass("active"); // Legger til klassen "active" til element med ID: "side2".
+		$(".side2").addClass("active"); // Legger til klassen "active" til element med ID: "side2".
 		$("#myNavbar").removeClass("in"); // Gjemmer dropdown-menyen ved menyvalg.
 		$("#slideshow").hide(); // Gjemmer slideshow
 		$(".col-sm-2").hide(); // Gjemmer sidekollonnen.
@@ -30,9 +30,9 @@ $(document).ready(function(){
 });
 // SIDE3
 $(document).ready(function(){
-    $("#side3").click(function(){
+    $(".side3").click(function(){
 		$("li").removeClass("active");
-		$("#side3").addClass("active");
+		$(".side3").addClass("active");
 		$("#myNavbar").removeClass("in");
 		$("#slideshow").hide();
 		$(".col-sm-2").show();
@@ -41,9 +41,9 @@ $(document).ready(function(){
 });
 // SIDE4
 $(document).ready(function(){
-    $("#side4").click(function(){
+    $(".side4").click(function(){
 		$("li").removeClass("active");
-		$("#side4").addClass("active");
+		$(".side4").addClass("active");
 		$("#myNavbar").removeClass("in");
 		$("#slideshow").show();
 		$(".col-sm-2").show();
@@ -52,9 +52,9 @@ $(document).ready(function(){
 });
 // SIDE5
 $(document).ready(function(){
-    $("#side5").click(function(){
+    $(".side5").click(function(){
 		$("li").removeClass("active");
-		$("#side5").addClass("active");
+		$(".side5").addClass("active");
 		$("#myNavbar").removeClass("in");
 		$("#slideshow").show();
 		$(".col-sm-2").show();
@@ -65,7 +65,7 @@ $(document).ready(function(){
 $(document).ready(function(){
     $(".skog").click(function(){
 		$("li").removeClass("active");
-		$("#side3").addClass("active");
+		$(".side3").addClass("active");
 		$("#slideshow").hide();
 		$(".col-sm-2").show();
         $("#div1").load("sidene/skog.html");
@@ -75,7 +75,7 @@ $(document).ready(function(){
 $(document).ready(function(){
     $(".fjell").click(function(){
 		$("li").removeClass("active");
-		$("#side3").addClass("active");
+		$(".side3").addClass("active");
 		$("#slideshow").hide();
 		$(".col-sm-2").show();
         $("#div1").load("sidene/fjell.html");
@@ -85,7 +85,7 @@ $(document).ready(function(){
 $(document).ready(function(){
     $(".sjø").click(function(){
 		$("li").removeClass("active");
-		$("#side3").addClass("active");
+		$(".side3").addClass("active");
 		$("#slideshow").hide();
 		$(".col-sm-2").show();
         $("#div1").load("sidene/sjø.html");
