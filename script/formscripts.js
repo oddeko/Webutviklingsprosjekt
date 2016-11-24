32e969077a437d9b911aa996f632c022f7a4654e
Sondre Grøneng <songro93@gmail.com>
2016-11-24 13:12:30 +0100

    Added some comments and moved around some lines to make it a bit more sensible.

--- script/formscripts.js
+++ script/formscripts.js
@@ -1,7 +1,16 @@
+// Note: This script is horribly written and I would love nothing more than to rewrite it from scratch
+// now that I have a better idea of what it actually needs to do. problem is, I dont dare to because you never
+// know what can go wrong when it comes to javascript and I do not have the time currently for any major setbacks.
+
 var hytteindex    = 0;
 var today         = new Date();
 var todayFormat   = formatDate(today)
 
+// A set of arrays holding all the information about the different cabins.
+// the order in which they appear can be read from the array with the imagenames since it mostly
+// uses the same name as the cabin itself. the only exception is fjell.jpg.
+// In most systems, this would be in a database and we would access it with,
+// most likely an ORM these days.
 var timepris      = [45,    55,    70,    40,    60,    75,    50,    65,    80];
 var dognpris      = [685,   790,   905,   640,   780,   895,   690,   890,   1025];
 var soverom       = [2,     2,     3,     2,     2,     3,     2,     3,     4];
@@ -14,30 +23,36 @@ var harBat        = [false, false, false, false, true,  true,  false, false, fal
 var bilder        = ["månetoppen.jpg", "tiurtoppen_stue.jpg", "fjell.jpg",
                      "fjordgløtt.jpg", "lykkeli.jpg", "nettbua.jpg",
                      "bjørnehiet.jpg", "furubo.jpg", "trollstua_stue.jpg"];
-
-var arrivalDate   = today;
-var departureDate = today;
-
-var antallDager   = 1;
-var antallTimer   = 0;
+// These would also in most cases be stored in a databse.
 var vaskepris     = 500;
 var medlemsrabatt = 0.15;
+
+// Information related to the order itself, barring personal information
+// since it is of no functional value to what we do.
+var arrivalDate   = today;
+var departureDate = today;
+var antallDager   = 1;
+var antallTimer   = 0;
 var totalpris     = 0;
-
-
-
-updatePriceForOneDayStay(0);
-
 
 var arrival = document.getElementById("arrival");
 var departure = document.getElementById("departure");
 
-arrival.setAttribute("min", todayFormat);
-arrival.value = todayFormat;
-departure.value = todayFormat;
-updateDeparturePicker(arrivalDate);
+updatePriceForOneDayStay(0); // Writes initial values to the screen.
+arrival.setAttribute("min", todayFormat); // Tries to ensure that you can not pick any dates before "today"
+                                          // Problem is, as we discovered, that by pressing the arrow buttons
+                                          // on the date input, in some browesers, or by pressing the up,
+                                          // or down, keys on the keyboard, you can blatantly bybass these restrictions
+                                          // Though when I think about it you can probably restrict that again by using a jquery
+                                          // .change method.
+arrival.value = todayFormat; //initally sets "today" as the currently selected arrival date.
+departure.value = todayFormat; // departure date too.
+updateDeparturePicker(arrivalDate); // changes the datepicker restrictions on the deaprture datefield to be within "today"
+                                    // and forward a forthnight.
 updateForm();
 
+// sets the restrictions on the departurepicker to be from "newArrivalDate" to
+// a forthnight after "newArrivalDate"
 function updateDeparturePicker(newArrivalDate) {
     // set the min date of departure to the same date as arrival
     var departureInput = document.getElementById("departure");
@@ -63,6 +78,8 @@ function formatDate(theDate) {
     return theDate;
 }
 
+// This function does exactly what it says on the tin. it updates the displayed price,
+// per hour, and total for all hours involved.
 function updatePriceForOneDayStay(index) {
     antallTimer = document.getElementById("antall-timer").value;
     document.getElementById("timepris").innerHTML = timepris[index];
@@ -79,15 +96,20 @@ function updateForm() {
     document.getElementById("output-døgnpris")  .innerHTML = dognpris[hytteindex];
     document.getElementById("output-soverom")   .innerHTML = soverom[hytteindex];
     document.getElementById("output-senger")    .innerHTML = senger[hytteindex];
-    document.getElementById("output-vann")      .innerHTML = harVann[hytteindex]   ? "Ja": "Nei";
-    document.getElementById("output-strøm")     .innerHTML = harStrom[hytteindex]  ? "Ja": "Nei";
-    document.getElementById("output-utedo")     .innerHTML = harUtedo[hytteindex]  ? "Ja": "Nei";
-    document.getElementById("output-wifi")      .innerHTML = harWifi[hytteindex]   ? "Ja": "Nei";
-    document.getElementById("output-båt")       .innerHTML = harBat[hytteindex]    ? "Ja": "Nei";
+    document.getElementById("output-vann")      .innerHTML = harVann[hytteindex]  ? "Ja": "Nei"; // Ternary operator, just in case, super compact if replacement.
+    document.getElementById("output-strøm")     .innerHTML = harStrom[hytteindex] ? "Ja": "Nei";
+    document.getElementById("output-utedo")     .innerHTML = harUtedo[hytteindex] ? "Ja": "Nei";
+    document.getElementById("output-wifi")      .innerHTML = harWifi[hytteindex]  ? "Ja": "Nei";
+    document.getElementById("output-båt")       .innerHTML = harBat[hytteindex]   ? "Ja": "Nei";
 
 }
 
+// calculates the fullprice based on current input and writes it to the screen.
+// I would honestly prefer to make this simply return the calculated price, perhaps based in parameters.
+// I probably could as well, but no reason to have jsut one sensible function in a script.
 function updateTotal() {
+    // decide whether to use hourly pricing, or dauly pricing, based on the
+    // number of days suggested by current input.
     if (antallDager == 1) {
         totalpris = timepris[hytteindex]*antallTimer;
     } else {
