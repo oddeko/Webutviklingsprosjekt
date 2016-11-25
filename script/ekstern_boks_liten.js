faad6ac665f4e03dc0978febb68b7d764832725c
oddeko <oddeko@stud.ntnu.no>
2016-11-25 18:25:27 +0100

    Pælma værmeldinga rett i index

--- script/ekstern_boks_liten.js
+++
@@ -1,3 +0,0 @@
-document.write('\n'
- + '<iframe src="http://www.yr.no/sted/Norge/Sogn_og_Fjordane/Gulen/Sognefjorden/ekstern_boks_liten.html" width="180" height="322" frameborder="0" style="margin: 10px 0 10px 0" scrolling="no"></iframe>\n'
-);
